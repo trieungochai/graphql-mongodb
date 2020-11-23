0833dbf5a398e6d613d50074828942702dc96f97
Macbook <trieungochai.dev@gmail.com>
2020-11-23 15:47:28 +0700

    Installing GraphQL Dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    })
+  ],
 })
 export class AppModule {}
